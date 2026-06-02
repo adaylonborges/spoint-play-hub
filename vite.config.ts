f7eb51d3c50b544580ee0a55a285ba47e367a771
adaylon.borges <adaylon.borges@gruposbf.com.br>
2026-06-02 17:17:58 -0300

    chore: add cloudflare vite plugin for tanstack start

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { cloudflare } from '@cloudflare/vite-plugin'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
+    cloudflare({
+      viteEnvironment: { name: 'ssr' }
+    }),
     tanstackStart({
-      server: { entry: "src/server.ts" }
+      server: { preset: 'cloudflare-pages' }
     }),
     react(),
     tailwindcss(),
